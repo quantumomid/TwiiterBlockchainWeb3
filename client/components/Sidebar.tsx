84074f84e98498a9da3c1aa5a06ac30d0a2d7381
quantumomid <omid.wakili@outlook.com>
2022-02-19 20:42:22 +0000

    use sidebaroptions with appropiate props i.e. icons etc

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -7,11 +7,18 @@ import { FaRegListAlt, FaHashtag, FaBell } from "react-icons/fa";
 import { CgMoreO } from "react-icons/cg";
 import { VscTwitter } from "react-icons/vsc";
 import SidebarOption from "./SidebarOption";
+import {
+    BsBookmark,
+    BsBookmarkFill,
+    BsPerson,
+    BsPersonFill,
+} from "react-icons/bs";
+import { useRouter } from "next/router";
 
 const style = {
     wrapper: "flex-[0.7] px-8 flex flex-col",
     twitterIconContainer: "text-3xl m-4",
-    tweetButton: "bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] px-[40px] mt-[20px] cursor-pointer",
+    tweetButton: "bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] px-8 mt-[20px] cursor-pointer",
     navContainer: "flex-1",
     profileButton: "flex items-center mb-6 cursor-pointer hover:bg-[#333c45] rounded-[100px] p-2",
     profileLeft: "flex items-center justify-center mr-4",
@@ -28,45 +35,86 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ initialSelectedIcon }) => {
-    const [selected, setSelected] = useState<String>(initialSelectedIcon)
+
+    const [selected, setSelected] = useState<String>(initialSelectedIcon);
+    const router = useRouter();
+
     return (
-    <header className={style.wrapper}>
-        <div className={style.twitterIconContainer}>
-            <VscTwitter />
-        </div>
-        <nav className={style.navContainer}>
-            <ul>
-                <SidebarOption 
-                    Icon={selected === "Home" ? RiHome7Fill : RiHome7Line}
-                    text="Home"
-                    isActive={Boolean(selected === "Home")}
-                    setSelected={setSelected}
-                    redirect={"/"}
-                />
-                <li>Explore</li>
-                <li>Notifications</li>
-                <li>Messages</li>
-                <li>Bookmarks</li>
-                <li>Lists</li>
-                <li>Profile</li>
-                <li>More</li>
-            </ul>
-            <button className={style.tweetButton}>Mint</button>
-        </nav>
-        <div className={style.profileButton}>
-            <div className={style.profileLeft}></div>
-            <div className={style.profileRight}>
-                <div className={style.details}>
-                    <div className={style.name}>Omid</div>
-                    <div className={style.handle}>@0sadid0</div>
-                </div>
-                <div className={style.moreContainer}>
-                    <FiMoreHorizontal />
+        <header className={style.wrapper}>
+            <div className={style.twitterIconContainer}>
+                <VscTwitter />
+            </div>
+            <nav className={style.navContainer}>
+                <ul>
+                    <SidebarOption 
+                        Icon={selected === "Home" ? RiHome7Fill : RiHome7Line}
+                        text="Home"
+                        isActive={Boolean(selected === "Home")}
+                        setSelected={setSelected}
+                        redirect={"/"}
+                    />
+                    <SidebarOption
+                        Icon={selected === 'Explore' ? FaHashtag : BiHash}
+                        text='Explore'
+                        isActive={Boolean(selected === 'Explore')}
+                        setSelected={setSelected}
+                    />
+                    <SidebarOption
+                        Icon={selected === "Notifications" ? FaBell : FiBell}
+                        text="Notifications"
+                        isActive={Boolean(selected === "Notifications")}
+                        setSelected={setSelected}
+                    />
+
+                    <SidebarOption
+                        Icon={selected === 'Messages' ? HiMail : HiOutlineMail}
+                        text='Messages'
+                        isActive={Boolean(selected === 'Messages')}
+                        setSelected={setSelected}
+                    />
+                    <SidebarOption
+                        Icon={selected === 'Bookmarks' ? BsBookmarkFill : BsBookmark}
+                        text='Bookmarks'
+                        isActive={Boolean(selected === 'Bookmarks')}
+                        setSelected={setSelected}
+                    />
+                    <SidebarOption
+                        Icon={selected === 'Lists' ? RiFileList2Fill : FaRegListAlt}
+                        text='Lists'
+                        isActive={Boolean(selected === 'Lists')}
+                        setSelected={setSelected}
+                    />
+                    <SidebarOption
+                        Icon={selected === 'Profile' ? BsPersonFill : BsPerson}
+                        text='Profile'
+                        isActive={Boolean(selected === 'Profile')}
+                        setSelected={setSelected}
+                        redirect={'/profile'}
+                    />
+
+                    <SidebarOption Icon={CgMoreO} text='More' />            
+                </ul>
+                <button 
+                    className={style.tweetButton}
+                    // onClick={() => router.push(`${router.pathname}/?mint=${currentAccount}`)}
+                >
+                    Mint
+                </button>
+            </nav>
+            <div className={style.profileButton}>
+                <div className={style.profileLeft}></div>
+                <div className={style.profileRight}>
+                    <div className={style.details}>
+                        <div className={style.name}>Omid</div>
+                        <div className={style.handle}>@0sadid0</div>
+                    </div>
+                    <div className={style.moreContainer}>
+                        <FiMoreHorizontal />
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-    )
+        </header>
+    );
 }
 
 export default Sidebar;
